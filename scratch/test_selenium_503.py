211fc69f45854b4427cba7e0ee923235163e17c2
luisvilriv <villegasriveraluis@gmail.com>
2026-05-22 12:26:16 +0200

    errores arreglados

--- scratch/test_selenium_503.py
+++ scratch/test_selenium_503.py
@@ -1,7 +1,7 @@
 import asyncio
 import sys
 import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from scraper.strategies.selenium_strategy import SeleniumStrategy
 from shared.auditor.auditor_modules.core import QualityAuditor
 
